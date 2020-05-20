b0ccf554d7d03392d74ab42cbae085324c44b9f1
Fabian Schmidt <fabis@outlook.com>
2020-05-20 13:16:28 +0200

    hourly google trend scraper for historical data

---
+++ trends_hourly_scraper.py
@@ -0,0 +1,15 @@
+# Since pytrends is returning a DataFrame object, we need pandas:
+import pandas as pd
+# Import of pytrends (needs to be pip installed first):
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+kw_list = ['Bitcoin', 'BTC']
+
+search_df = pytrends.get_historical_interest(kw_list, year_start=2020,
+                                             month_start=2, day_start=29,
+                                             hour_start=0, year_end=2020,
+                                             month_end=5, day_end=19, hour_end=0,
+                                             cat=0, geo='', gprop='', sleep=60)
+
+search_df.to_csv("data/BTC_trend.csv")
