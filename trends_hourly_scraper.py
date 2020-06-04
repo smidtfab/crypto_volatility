9a431c8472a1b14b0aa80644b2c6922aa8f33838
Fabian Schmidt <fabis@outlook.com>
2020-06-04 15:30:20 +0200

    google trend scraper

--- trends_hourly_scraper.py
+++ trends_hourly_scraper.py
@@ -7,9 +7,9 @@ pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ['Bitcoin', 'BTC']
 
 search_df = pytrends.get_historical_interest(kw_list, year_start=2020,
-                                             month_start=2, day_start=29,
+                                             month_start=1, day_start=1,
                                              hour_start=0, year_end=2020,
-                                             month_end=5, day_end=19, hour_end=0,
+                                             month_end=5, day_end=31, hour_end=0,
                                              cat=0, geo='', gprop='', sleep=60)
 
 search_df.to_csv("data/BTC_trend.csv")
