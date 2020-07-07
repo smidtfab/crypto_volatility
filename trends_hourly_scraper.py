5cc758182132ee0c24fa07d1c0c673b6683b20db
Fabian Schmidt <fabis@outlook.com>
2020-07-07 12:19:12 +0200

    params for complete time frame

--- trends_hourly_scraper.py
+++ trends_hourly_scraper.py
@@ -7,9 +7,9 @@ pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ['Bitcoin', 'BTC']
 
 search_df = pytrends.get_historical_interest(kw_list, year_start=2020,
-                                             month_start=1, day_start=1,
+                                             month_start=1, day_start=31,
                                              hour_start=0, year_end=2020,
-                                             month_end=5, day_end=31, hour_end=0,
+                                             month_end=6, day_end=1, hour_end=0,
                                              cat=0, geo='', gprop='', sleep=60)
 
-search_df.to_csv("data/BTC_trend.csv")
+search_df.to_csv("data/BTC_trend_complete.csv")
